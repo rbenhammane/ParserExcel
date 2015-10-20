8435405bf6c1fad8e3aceff68d8f8c651dcbb856
Salmane Sentissi <salmane.sentissi@gmail.com>
2015-10-20 16:32:34 +0200

    Conflicts

--- src/ma/ericsson/granite/cli/Launcher.java
+++ src/ma/ericsson/granite/cli/Launcher.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import ma.ericsson.granite.cli.exception.GUIParserException;
 import ma.ericsson.granite.cli.exception.GUIParserInputException;
+import ma.ericsson.granite.cli.model.Form;
 import ma.ericsson.granite.cli.model.GUI;
 import ma.ericsson.granite.cli.service.GUIBuilder;
 import ma.ericsson.granite.cli.service.SRMSParser;
@@ -34,78 +35,42 @@ public class Launcher {
 	public static void main(String[] args) {
 
 		// String file = "SRMS_ScheduleAndEffort_v02_formatted.xlsx";
-		String file = "20151007 SRMS User interracrion Specification_formatted.xlsx";
+		String file = "gui.xlsx";
 
 		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationResources.xml", "applicationContext.xml" });
 
-		SRMSParser<InputStream, List<GUI>> parser = (SRMSParser<InputStream, List<GUI>>) context.getBean("parser");
+		SRMSParser<InputStream, Form> parser = (SRMSParser<InputStream, Form>) context.getBean("singleGUIParser");
 
-		GUIBuilder<List<GUI>, Map<String, List<String>>> builder = (GUIBuilder<List<GUI>, Map<String, List<String>>>) context.getBean("builder");
+		GUIBuilder<Form, List<String>> builder = (GUIBuilder<Form, List<String>>) context.getBean("singleFormBuilder");
 
-		List<GUI> guis;
-		Map<String, List<String>> output;
+		Form form;
+		List<String> output;
 
 		try {
 			FileInputStream in = new FileInputStream(file);
-			guis = parser.parseGUIs(in);
+			form = parser.parseGUIs(in);
 
-			for (GUI gui : guis) {
-				try {
-					SRMSConstantsGenerator.createConstants(gui);
-					SRMSJspGenerator.createJSP(gui);
-					SRMSModelGenerator.createClassModel(gui);
-					SRMSServiceGenerator.createClassService(gui);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+//			try {
+//				SRMSConstantsGenerator.createConstants(form);
+//				SRMSJspGenerator.createJSP(form);
+//				SRMSModelGenerator.createClassModel(form);
+//				SRMSServiceGenerator.createClassService(form);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+
+//			SRMSConstantsGenerator.close();
+
+			output = builder.build(form);
+
+			PrintWriter writer = new PrintWriter("gui.sql");
+
+			for (int i = 0; i < output.size(); i++) {
+				writer.write(output.get(i));
 			}
-			SRMSConstantsGenerator.close();
 
-			output = builder.build(guis);
-			
-			PrintWriter writerAllSQL = new PrintWriter("guis/gui_all_EAT_INSERT.sql");;
-			PrintWriter writer;
-			for (String key : output.keySet()) {
-				if (key.contains("Delete")) {
-					writer = new PrintWriter("guis/gui_" + key + ".sql");
-					
-					for (int i = 0; i < output.get(key).size(); i++) {
-						writer.write(output.get(key).get(i));
-						writerAllSQL.write(output.get(key).get(i));
-					}
-
-					writer.flush();
-					writer.close();
-
-				}
-				else if (key.contains("GraniteView")) {
-					writer = new PrintWriter("guis/gui_" + key + ".sql");
-					
-					for (int i = 0; i < output.get(key).size(); i++) {
-						writer.write(output.get(key).get(i));
-						writerAllSQL.write(output.get(key).get(i));
-					}
-
-					writer.flush();
-					writer.close();
-
-				}
-				else {
-					writer = new PrintWriter("guis/gui_" + key + ".sql");
-					
-					for (int i = 0; i < output.get(key).size(); i++) {
-						writer.write(output.get(key).get(i));
-						writerAllSQL.write(output.get(key).get(i));
-					}
-
-					writer.flush();
-					writer.close();
-
-				}
-			}
-			
-			writerAllSQL.flush();
-			writerAllSQL.close();
+			writer.flush();
+			writer.close();
 
 		} catch (GUIParserInputException e) {
 			String errorFile = file.substring(0, file.lastIndexOf('.')) + "_errors.xlsx";
